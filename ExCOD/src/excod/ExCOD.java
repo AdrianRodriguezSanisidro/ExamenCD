6108ae4c4782da0a9505c554f78ea143b0292884
Adry <Adry@Sanisidro>
2016-11-10 08:49:58 +0100

    Pregunto algo con System out println

--- ExCOD/src/excod/ExCOD.java
+++ ExCOD/src/excod/ExCOD.java
@@ -8,6 +8,7 @@ public class ExCOD {
 
     public static void main(String[] args) {
         System.out.println("Hola");//Muestro por pantalla un saludo
+        System.out.println("Que tal?");//Pregunto algo
     }
     
 }
